d71c506a9640c7e3d6c96b9e2259aa5923f88f5b
Vladimir <fayst_@bk.ru>
2020-08-25 15:49:49 +0300

    Переработал шаблон, добавил состояние, поработал над кастомными событиями.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,11 +5,56 @@ import Product from '../product/Product';
 
 export default class App extends Component {
 
+  state = {
+    productsData: [
+      {
+        id: 1,
+        taste: 'с фуа-гра',
+        proposal: '10 порций мышь\u00A0в\u00A0подарок',
+        weight: '0.5кг',
+        active: false,
+        disabled: false
+      },
+      {
+        id: 2,
+        taste: 'с рыбой',
+        proposal: '40 порций 2\u00A0мыши\u00A0в подарок',
+        weight: '2кг',
+        active: true,
+        disabled: false
+      },
+      {
+        id: 3,
+        taste: 'с курой',
+        proposal: '100 порций 5\u00A0мышей в подарок заказчик доволен',
+        weight: '5кг',
+        active: false,
+        disabled: true
+      }
+    ]    
+  }
+
+  clickProduct = (id) => {
+    this.setState(({productsData}) => {
+    const newProductsData = productsData.map(el =>{
+      if (el.id === id) el.active = !el.active;
+      return el
+    });
+
+    return {productsData: newProductsData}    
+    });
+
+    
+  }
+
   render() {
+    const { productsData } = this.state;
     return (
       <div className = 'main'>
-        <h1 class="main-title">Ты сегодня покормил кота?</h1>
-        <Product />
+        <h1 className="main-title">Ты сегодня покормил кота?</h1>
+        <Product
+          product = { productsData }
+          click = { this.clickProduct }/>
       </div>
     );
   }
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './product.css';
 
-export default class Product extends Component {
+const Product = ({product, click}) =>  {   
 
-  render () {
+  const products = product.map(({taste, proposal, id, active, disabled}) => {
+    
+    let nameClassActive = active ? 'block_product-product--active' : 'block_product-product--unactive';      
+    if (disabled) nameClassActive = 'block_product-product--disabled';
+
+    const onHedlerProduct = (evt) => {
+      const hendlerMouseleave = (e) => {        
+        click(id);
+        e.target.removeEventListener('mouseleave', hendlerMouseleave);       
+      }  
+      evt.target.addEventListener('mouseleave', hendlerMouseleave);       
+    }
+    
     return (
-    <div class="main-block_product">
-
-      <div class="block_product">
-        <div class="block_product-product block_product-product--unactive">
-          <p class="product-title">Сказочное заморское яство</p>
-          <h2 class="product-brend">Нямушка</h2>
-          <p class="product-taste">с фуа-гра</p>
-          <p class="product-bonus">10 порций мышь{'\u00a0'}в{'\u00a0'}подарок</p>
+      <div className="block_product"
+           key = { id }
+           onClick = { onHedlerProduct  }
+      >
+        <div className =  { `block_product-product ${ nameClassActive } ` }>
+          <p className = "product-title">Сказочное заморское яство</p>
+          <h2 className = "product-brend">Нямушка</h2>
+          <p className = "product-taste">{ taste }</p>
+          <p className = "product-bonus">{ proposal }</p>
         </div>
-        <span class="block_product-post">Чего сидишь? Порадуй котэ, <a href='/' class="block_product-post_link">купи.</a></span>
+        <span className = "block_product-post">Чего сидишь? Порадуй котэ, <a href='/' className="block_product-post_link">купи.</a></span>
       </div>
-
-      <div class="block_product">
-        <div class="block_product-product block_product-product--active">
-          <p class="product-title">Сказочное заморское яство</p>
-          <h2 class="product-brend">Нямушка</h2>
-          <p class="product-taste">с рыбой</p>
-          <p class="product-bonus">40 порций 2{'\u00a0'}мыши{'\u00a0'}в подарок</p>
-        </div>
-        <span class="block_product-post">Головы щучьи с чесноком да свежайшая сёмгушка.</span>
-      </div>
-
-      <div class="block_product">
-        <div class="block_product-product block_product-product--disabled">
-          <p class="product-title">Сказочное заморское яство</p>
-          <h2 class="product-brend">Нямушка</h2>
-          <p class="product-taste">с курой</p>
-          <p class="product-bonus">100 порций 5{'\u00a0'}мышей в подарок заказчик доволен</p>
-        </div>
-        <span class="block_product-post block_product-post--disabled">Печалька, с курой закончился.</span>
-      </div>      
-    </div>
     );
-  }
-}
+  });
+
+  return (
+    <div className="main-block_product">
+      { products }      
+    </div>
+  );
+ }
+
+export default Product;
