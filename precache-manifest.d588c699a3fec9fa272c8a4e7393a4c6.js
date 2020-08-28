20b7019edefd3b27d912833ec60baf9f878da450
Vladimir <fayst_@bk.ru>
2020-08-28 21:05:03 +0300

    Updates

---
+++ precache-manifest.d588c699a3fec9fa272c8a4e7393a4c6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee19fcf51ad2abfd360dbf2999f80846",
+    "url": "/FunBox_test/index.html"
+  },
+  {
+    "revision": "764a3658a1192de30014",
+    "url": "/FunBox_test/static/css/main.d4b75b12.chunk.css"
+  },
+  {
+    "revision": "96400ad7ca1738d575c5",
+    "url": "/FunBox_test/static/js/2.cf4aa470.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FunBox_test/static/js/2.cf4aa470.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764a3658a1192de30014",
+    "url": "/FunBox_test/static/js/main.4f1db347.chunk.js"
+  },
+  {
+    "revision": "303968a16a5441c76165",
+    "url": "/FunBox_test/static/js/runtime-main.96d3ac3c.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/FunBox_test/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "3d7e1ad70582e5c2e1e79c70f54739ad",
+    "url": "/FunBox_test/static/media/Pattern.3d7e1ad7.png"
+  },
+  {
+    "revision": "7afeaf2b8e5fee8b3332318a9f216092",
+    "url": "/FunBox_test/static/media/TrebuchetMS-Bold.7afeaf2b.ttf"
+  },
+  {
+    "revision": "530d3166bc7f3ee970eec68df2a2bd2e",
+    "url": "/FunBox_test/static/media/TrebuchetMS.530d3166.ttf"
+  },
+  {
+    "revision": "edad387cce5ff1d6d7ebe25e5f5a6319",
+    "url": "/FunBox_test/static/media/cat_active.edad387c.png"
+  },
+  {
+    "revision": "a070f24b3b4ea099c14ff55628800b0a",
+    "url": "/FunBox_test/static/media/cat_active_hover.a070f24b.png"
+  },
+  {
+    "revision": "95ed1a4e5f36e92adb6450c7c54a361b",
+    "url": "/FunBox_test/static/media/cat_disabled.95ed1a4e.png"
+  },
+  {
+    "revision": "9c44a2b92a7299df245229d9f2b7f75c",
+    "url": "/FunBox_test/static/media/cat_unactive.9c44a2b9.png"
+  },
+  {
+    "revision": "499c42661b9fb2c7d63db3054b451a11",
+    "url": "/FunBox_test/static/media/cat_unactive_hover.499c4266.png"
+  }
+]);
