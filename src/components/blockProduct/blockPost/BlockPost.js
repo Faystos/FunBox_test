a8616512326762b74053b052d01af3a61d075fe4
Vladimir <fayst_@bk.ru>
2020-08-28 20:03:44 +0300

    Добавил пробел в тексте.

--- src/components/blockProduct/blockPost/BlockPost.js
+++ src/components/blockProduct/blockPost/BlockPost.js
@@ -6,16 +6,11 @@ const BlockPost = ({ description, taste, clickProduct, id }) => {
     evt.preventDefault();
     clickProduct(id)
   }
-  
+
   return (
     <div className = "block_product-block_post">
-      <span className = "block_post-post_unactive">Чего сидишь? Порадуй котэ,
-       <a href='/'
-        className = "block_product-post_link"
-        onClick = { onHendlerLink }
-       >
-          купи.
-        </a>
+      <span className = "block_post-post_unactive">
+        Чего сидишь? Порадуй котэ, <a href='/' className = "block_product-post_link" onClick = { onHendlerLink }>купи.</a>
       </span>
       <span className = "block_post-post_active">{ description }</span>
       <span className = "block_post-post_disabled">Печалька, { taste } закончился.</span>
